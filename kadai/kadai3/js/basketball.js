73af8bd38604b74e176f906b164ec65f97c58a34
ReadAlpha <ReadAlpha@uncleSam.local>
2019-05-19 16:51:38 +0900

    Gitにファイル登録

---
+++ kadai/kadai3/js/basketball.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function () {
+  $(".blooklyn_slide").owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: true,
+    navText: [
+      '<span aria-label="Previous"</span>',
+      '<span aria-label="Next"</span>'
+    ],
+    responsive: {
+      0: {
+        nav: false,
+        dots: true
+      },
+      960: {
+        nav: true,
+        dots: false
+      }
+    }
+  });
+});
+
+/********ボタンが生えてくるアニメーション******/
+
+$(function () {
+  $(".others .button").css("opacity", "0");
+  $(window).scroll(function () {
+    $(".others__contents").each(function () {
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+
+      if (scroll > imgPos - windowHeight + windowHeight / 4) {
+        console.log(".othersの条件成立");
+        $(".button", this).css("opacity", "1");
+        $(".button", this).css("bottom", "10%");
+        //$(".section_textContent").css("top", "50%");
+      } else {
+        $(".button", this).css("opacity", "0");
+      }
+    });
+  });
+});
+$(function () {
+  $(".section_textContent").css("opacity", "0");
+  $(window).scroll(function () {
+    $(".section_textContent").each(function () {
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+
+      if (scroll > imgPos - windowHeight + windowHeight / 10) {
+        var temp = imgPos - windowHeight;
+        $(this).css("opacity", "1");
+        $(this).css("top", "50%");
+      }
+      else {
+        $(this).css("opacity", "0");
+      }
+    });
+  });
+});
